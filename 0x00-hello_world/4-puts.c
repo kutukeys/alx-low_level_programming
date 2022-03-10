1fa911e17e4bde115a4d1560e51e0a644675e657
kutukeys <israelkutu@gmail.com>
2022-03-10 09:07:01 -0800

    puts commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
  * main - Entry point
- * 
- * Return:Always 0 (success)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like a multilingual puzzle");
 	return (0);
 }
