16cde65f322cbdfdfd5da8e88fa66e8a5394bb3d
kutukeys <israelkutu@gmail.com>
2022-03-11 13:36:38 -0800

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints number between 0 to 9.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 20; i < 30; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
