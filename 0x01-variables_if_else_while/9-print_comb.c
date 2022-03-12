a32f315b966432052d37f8035fd9cbc466bbf29e
kutukeys <israelkutu@gmail.com>
2022-03-11 21:59:33 -0800

    committed

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-	int num;
+	int number;
 
-	for (num = 0; num <= 9; num++)
+	for (number = 0; number <= 9; number++)
 
 	{
-	putchar(num + 0);
-	if (num < 9)
+	putchar(number + 0);
+	if (number < 9)
 	{
 		putchar(',');
 		putchar(' ');
