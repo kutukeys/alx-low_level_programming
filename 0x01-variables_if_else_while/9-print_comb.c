25989f0945906f86f64c6fc37eaf870bbe819ec5
kutukeys <israelkutu@gmail.com>
2022-03-11 21:19:58 -0800

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Print all possible combinatiions of single digit numbers
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+	putchar(num + '0');
+	if (num < '9')
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	putchar('\n');
+
+	return (0);
+}
