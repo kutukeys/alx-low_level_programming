f685a7a03e5e4536c36486327e1c3bb35f935af1
israel oluwatosin kutu <67150336+kutukeys@users.noreply.github.com>
2022-03-12 13:33:07 +0100

    update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
 
 /**
- * main - Print all possible combinations of single digit numbers
- *
- * Return: Always return 0.
- */
+* main - Print combinations of two digit numbers
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	int tens;
-	int ones;
+  int tens;
+  int ones;
 
-	for (tens = 0; tens <= 9; tens++)
+  for (tens = 0; tens <= 9; tens++)
+    {
+      for (ones = tens + 1; ones <= 9; ones++)
 	{
-		for (ones = tens + 1; ones <= 9; ones++)
-		{
-			putchar(tens + '0');
-			putchar(ones + '0');
+	  putchar(tens + '0');
+	  putchar(ones + '0');
 
-		 	if (tens < 8)
-		 	{
-		 		putchar(',');
-			 	putchar(' ');
-		 	}
-		}
+	  if (tens < 8)
+	    {
+	      putchar(',');
+	      putchar(' ');
+	    }
 	}
-	putchar('\n');
-	return (0);
+    }
+  putchar('\n');
+
+  return (0);
 }
