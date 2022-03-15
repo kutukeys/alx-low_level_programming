407b9cafc4b96a02aa3f6341455f1ce7c04d7190
kutukeys <israelkutu@gmail.com>
2022-03-15 10:17:47 -0700

    Done

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - Print if it's lowercase
+ * @c: integer
+ * Description: Print lowercase alphabet
+ * Return: Nothing
+ */
+
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
