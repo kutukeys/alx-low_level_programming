9d1caec841411736475925924acd995be4dc2890
kutukeys <israelkutu@gmail.com>
2022-03-15 09:54:40 -0700

    Done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - check the code
+ * Return: Always (0)
+ */
+
+void print_alphabet_x10(void)
+{
+	int letters = 'a', l = 0;
+
+	while (l < 10)
+	{
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+
+		letter = 'a';
+
+		_putchar('\n');
+		l++;
+	}
+}
+
