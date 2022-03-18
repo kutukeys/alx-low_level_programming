4468c723e182c32a205ed3e8d4ab158f64a28a40
kutukeys <israelkutu@gmail.com>
2022-03-18 06:37:02 -0700

    committed

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbersw from 0 to 9
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int i = '0';
+
+	for (; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
