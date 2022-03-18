99de102615c084b2af67cbf5f5ee05d1dffd9d03
kutukeys <israelkutu@gmail.com>
2022-03-18 06:11:56 -0700

    committed

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers from 0 to 9
+ * print_numbers - prints numbers 0-9
  *
- * Return: 0
+ * Return: void
  */
 
 void print_numbers(void)
 {
 	int c;
 
-	for (c = '0'; c < '10'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
 	}
