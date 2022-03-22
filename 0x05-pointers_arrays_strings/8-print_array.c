9a22c61463b82ef3b813e000e329504bef9ac74a
kutukeys <israelkutu@gmail.com>
2022-03-21 19:27:40 -0700

    Dusted

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - prints number of arrays
+ * @a: number
+ * @n: number
+ * Return: no return
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
+}
